e839d0e19e980c4580ba06d498d23f2e6e61f0c5
avery bertrand <averwhy@gmail.com>
2025-04-07 15:05:33 -0400

    Added one test poggers

--- src/tests/Dropdown.test.jsx
+++ src/tests/Dropdown.test.jsx
@@ -1,15 +1,22 @@
 import { expect, test } from 'vitest';
 import { render } from '@testing-library/react';
 import { it, describe } from 'vitest';
+import { BrowserRouter } from "react-router-dom";
+import Dropdown from "../components/Dropdown";
 import React from 'react';
 import ThemeDrop from '../components/Dropdown';
 
 import { getItemById } from './helpers';
 
-describe('ThemeDrop', () => {
-  it('renders the dropdown component', 
-    getItemById(<ThemeDrop />, 'dropdown')
-  );
+describe('Theme Dropdown', () => {
+  it('renders the dropdown component', () => {
+    render(
+      <BrowserRouter>
+        <Dropdown />
+      </BrowserRouter>
+    );
+    expect(document.getElementById("dropdown-basic") !== null).toBeTruthy();
+  });
 
 //   it('toggles the dropdown menu when clicked', () => {
 //     render(<ThemeDrop />);
